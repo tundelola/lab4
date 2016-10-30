afc57d851a385cd755af77c00ba7fdea3cf99104
tundelola <foladayo@aol.com>
2016-10-30 17:02:12 -0400

    Innitial Program

---
+++ calcFactorial/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcFactorial
+{
+    //This Program Calculates the factorial of a number input by the user
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+
+            while (num >= 0)
+            {
+
+                Console.WriteLine("Welcome to the Factorial Calculator!");
+            
+                Console.WriteLine("Please Enter an interger number greater than 0 but less than 10");//Gets user's input
+                num = int.Parse(Console.ReadLine());//Reads Users input
+
+
+                long output = calcFactorial(num);//Fuction call to calculate factorial of numbers input by the users
+                Console.WriteLine("The Factorial of Number = " + output); //output the factorial of user's input
+
+                Console.WriteLine("Do you want to continue? ");
+                string userInput = Console.ReadLine();//confirm whether users wants to continue and reads the input
+                if (userInput == "No")
+                {
+                    break;
+
+                }
+            }
+           
+        }
+        // Method calculates the factorial of user's input
+        public static long calcFactorial(int num)
+        {
+            long result = 1;
+            for (long i = 1; i <= num; i++)
+            {
+                result = result * i;
+
+            }
+            return result;
+
+        }
+    }
+
+}
+
+
+
+
---
+++ factorialTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace factorialTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestThatFactorial5Is120()
+        {
+            long result = calcFactorial.Program.calcFactorial(5);
+            Assert.AreEqual(120, result);
+        }
+
+        [TestMethod]
+        public void TestThatFactorial5Is1()
+        {
+            long result = calcFactorial.Program.calcFactorial(1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestThatFactorial5Is2()
+        {
+            long result = calcFactorial.Program.calcFactorial(2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestThatFactorial5Is3()
+        {
+            long result = calcFactorial.Program.calcFactorial(3);
+            Assert.AreEqual(6, result);
+        }
+
+
+        [TestMethod]
+        public void TestThatFactorial5Is8()
+        {
+            long result = calcFactorial.Program.calcFactorial(8);
+            Assert.AreEqual(40320, result);
+        }
+
+
+        [TestMethod]
+        public void TestThatFactorial5Is7()
+        {
+            long result = calcFactorial.Program.calcFactorial(7);
+            Assert.AreEqual(5040, result);
+        }
+
+
+        [TestMethod]
+        public void TestThatFactorial5Is6()
+        {
+            long result = calcFactorial.Program.calcFactorial(6);
+            Assert.AreEqual(720, result);
+        }
+
+    }
+}
